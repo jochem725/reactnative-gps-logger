9f6826eb0e806885643f014e1789a7506738f38d
jochem725 <jochemlugtenburg@gmail.com>
2017-04-25 22:05:31 +0200

    Add LocationSamplerComponent to iOS and Android

--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,7 @@
  */
 
 import React, { Component } from 'react';
+import LocationSampler from 'built/LocationSamplerComponent'
 import {
   AppRegistry,
   StyleSheet,
@@ -15,39 +16,11 @@ import {
 export default class GPSLogger extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View>
+        <LocationSampler />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('GPSLogger', () => GPSLogger);
---
+++ js/LocationSamplerComponent.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {
+    View
+} from 'react-native';
+
+interface LocationSamplerProps {
+
+}
+
+export default class LocationSamplerComponent extends React.Component<undefined, undefined> {
+    public render() {
+        return (
+            <View>
+                
+            </View>
+        );
+    }
+}
